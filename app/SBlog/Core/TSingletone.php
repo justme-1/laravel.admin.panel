20d749c767e80d0592ad90e9ca763fe98750d040
justme-1 <61352725+justme-1@users.noreply.github.com>
2020-03-28 22:33:22 +0300

    create Tsingleton, Registry,BlogApp

--- app/SBlog/Core/TSingletone.php
+++ app/SBlog/Core/TSingletone.php
@@ -6,5 +6,12 @@ namespace App\SBlog\Core;
 
 trait TSingletone
 {
+    public static  $instance;
 
+    public static function instance(){
+        if(self::$instance===null){
+            self::$instance=new self;
+        }
+        return self::$instance;
+    }
 }
